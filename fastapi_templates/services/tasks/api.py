1be03c2309dc17a1daed12fbb524f03d9b203970
waitold <chakkggm@gmail.com>
2024-05-01 11:06:29 +0900

    fix: typo

--- fastapi_templates/services/tasks/api.py
+++ fastapi_templates/services/tasks/api.py
@@ -3,5 +3,5 @@ from sqlmodel import Session
 from .repository import TasksRepository
 
 
-def get_tasks_repositorys(session: Session) -> TasksRepository:
+def get_tasks_repository(session: Session) -> TasksRepository:
     return TasksRepository(session)
--- fastapi_templates/services/tasks/views.py
+++ fastapi_templates/services/tasks/views.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from fastapi_templates.db import get_session
 from fastapi_templates.exception import ResourceDoesNotExistError
-from fastapi_templates.services.tasks.api import get_tasks_repositorys
+from fastapi_templates.services.tasks.api import get_tasks_repository
 from fastapi_templates.services.tasks.models import Tasks
 from fastapi_templates.services.tasks.schemas import TasksCreate, TasksRead, TasksUpdate
 
@@ -11,20 +11,20 @@ router = APIRouter()
 
 @router.get("/", response_model=list[Tasks])
 def list_tasks(limit=20, offset=0, session=Depends(get_session)):
-    repo = get_tasks_repositorys(session)
+    repo = get_tasks_repository(session)
     tasks = repo.list_tasks(limit, offset)
     return tasks
 
 
 @router.post("/", response_model=TasksRead)
 def create_task(task: TasksCreate, session=Depends(get_session)):
-    repo = get_tasks_repositorys(session)
+    repo = get_tasks_repository(session)
     return repo.create_task(task)
 
 
 @router.get(path="/{id}", response_model=TasksRead)
 def get_task(id: int, session=Depends(get_session)):
-    repo = get_tasks_repositorys(session)
+    repo = get_tasks_repository(session)
     try:
         task = repo.get_task(id)
     except ResourceDoesNotExistError:
@@ -34,7 +34,7 @@ def get_task(id: int, session=Depends(get_session)):
 
 @router.delete(path="/{id}")
 def delete_task(id: int, session=Depends(get_session)):
-    repo = get_tasks_repositorys(session)
+    repo = get_tasks_repository(session)
     try:
         repo.delete_task(id)
     except ResourceDoesNotExistError:
@@ -43,7 +43,7 @@ def delete_task(id: int, session=Depends(get_session)):
 
 @router.patch(path="/{id}", response_model=TasksRead)
 def update_task(id: int, task: TasksUpdate, session=Depends(get_session)):
-    repo = get_tasks_repositorys(session)
+    repo = get_tasks_repository(session)
     updated_task = repo.update_task(id, task)
     if updated_task is None:
         return HTTPException(status_code=404, detail="Task not found")
